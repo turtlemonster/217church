0b29f192d05767f4daa31a6f8aa5036a9eebe33d
Moo Hyun Kim <moo@halopowered.com>
2015-05-21 01:19:32 -0400

    - hover styles test js added - mission and stories section started

---
+++ js/217church__site.js
@@ -0,0 +1,25 @@
+(function() {
+
+
+    //detect mouse movement to test if we should load hover styles
+    var hoverListener = function () {
+        document.removeEventListener('mousemove', hoverListener, false);
+        
+        var cssId = 'hoverStyles';
+        if (!document.getElementById(cssId)) {
+            var head  = document.getElementsByTagName('head')[0];
+            var link  = document.createElement('link');
+            link.id   = cssId;
+            link.rel  = 'stylesheet';
+            link.type = 'text/css';
+            link.href = 'css/217church__hover.css';
+            link.media = 'screen';
+            head.appendChild(link);
+        }
+    };
+
+    document.addEventListener('mousemove', hoverListener, false);
+
+
+
+})();
