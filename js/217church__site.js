f6f8a9b00956c5b59ef3c9ef5d007418ea21e018
Moo H Kim <moo@halopowered.com>
2015-05-28 17:50:58 -0400

    - mobile nav added - hover effects added for campus links

--- js/217church__site.js
+++ js/217church__site.js
@@ -1,4 +1,4 @@
-(function() {
+$(function() {
 
 
     //detect mouse movement to test if we should load hover styles
@@ -21,5 +21,20 @@
     document.addEventListener('mousemove', hoverListener, false);
 
 
+    //dock nav to top on scroll
+    var $mainNav            = $('.mainNav__group'),
+        $intro              = $('.introSection'),
+        triggerHeight       = $('.campusLinks__group').height();
 
-})();
+    $(document).scroll( function() {
+        if( $('body').scrollTop() > triggerHeight ) {
+            $mainNav.addClass('mainNav__group--docked');
+            $intro.addClass('introSection--navDocked');
+        } else {
+            $mainNav.removeClass('mainNav__group--docked');
+            $intro.removeClass('introSection--navDocked');
+        }
+    });
+
+
+});
